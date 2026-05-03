151b0aed6f9cb524387936bad9f4c3b79913552f
terrocar <paraweonadas1994@gmail.com>
2026-05-03 13:17:14 -0300

    Update wiki final

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lostrem.github.io/Reinos-Olvidados",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
